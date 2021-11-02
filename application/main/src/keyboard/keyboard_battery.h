96cd173aff0e4acfced8e828c416d1bb669733fa
Geno [格诺] <genokolar@gmail.com>
2021-11-02 13:11:49 +0800

    解决电池电量信息传递事件无法运行问题

--- application/main/src/keyboard/keyboard_battery.h
+++ application/main/src/keyboard/keyboard_battery.h
@@ -3,15 +3,15 @@
 
 #define ADC_BUFFER_SIZE 6
 
-struct BatteryInfo {
+typedef struct {
     uint16_t voltage; // 电压 (mv)
     uint8_t percentage; // 百分比
-};
+}battery_info_t ;
 
 /**
  * 电量信息
  */
-extern struct BatteryInfo battery_info;
+extern battery_info_t battery_info;
 
 /**@brief 输出电池剩余电量.
  *
